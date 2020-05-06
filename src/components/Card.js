d7ef8fde12d368890c218a0c31072519ac1d44af
benaor <benjamingirard25@gmail.com>
2020-05-06 23:30:15 +0200

    display default img if error on loading img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const Card = ({ details }) => {
+
     const ingredients = details.ingredients.split(',')
         .map(item => <li key={item} >{item}</li>)
 
     const instructions = details.instructions.split('\n')
         .map(item => <li key={item} >{item}</li>)
 
+    const requireImage = chemin => {
+        try {
+            return require(`../img/${chemin}`)
+        } catch (err) {
+            return require(`../img/default.jpeg`)
+        }
+    }
+
     return (
         <div className='card'>
             <div className="image">
-                <img src={require(`../img/${details.image}`)} alt={details.nom} />
+                <img src={requireImage(details.image)} alt={details.nom} />
             </div>
             <div className="recette">
                 <h2>{details.nom}</h2>
