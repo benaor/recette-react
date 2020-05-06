70cdeb42f0ee3c5c8bce846e2e65e896982c9946
benaor <benjamingirard25@gmail.com>
2020-05-06 23:24:08 +0200

    stylise Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 const Card = ({ details }) => {
+    const ingredients = details.ingredients.split(',')
+        .map(item => <li key={item} >{item}</li>)
+
+    const instructions = details.instructions.split('\n')
+        .map(item => <li key={item} >{item}</li>)
+
     return (
         <div className='card'>
+            <div className="image">
+                <img src={require(`../img/${details.image}`)} alt={details.nom} />
+            </div>
             <div className="recette">
                 <h2>{details.nom}</h2>
+                <ul className="liste-ingredients">
+                    {ingredients}
+                </ul>
+                <ol className="liste-instructions">
+                    {instructions}
+                </ol>
             </div>
         </div>
     )
