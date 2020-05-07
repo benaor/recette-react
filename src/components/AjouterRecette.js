d2c6583f19b95f028d564575f9ebf0c203c78be2
benaor <benjamingirard25@gmail.com>
2020-05-07 23:59:30 +0200

    add submit button

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -13,14 +13,21 @@ class AjouterRecette extends Component {
         this.setState({ [name]: value })
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+        const recette = { ... this.state }
+        this.props.AjouterRecette(recette)
+    } 
+
     render() {
         return (
             <div className="card">
-                <form className='admin-form ajouter-recette'>
+                <form className='admin-form ajouter-recette' onSubmit={this.handleSubmit}>
                     <input value={this.state.nom} onChange={this.handleChange} type="text" name="nom" placeholder="Nom de la recette" />
                     <input value={this.state.image} onChange={this.handleChange} type="text" name="image" placeholder="nom de l'image" />
                     <textarea value={this.state.ingredients} onChange={this.handleChange} rows="3" name="ingredients" placeholder="liste des ingrédients"></textarea>
                     <textarea value={this.state.instructions} onChange={this.handleChange} rows="15" name="instructions" placeholder="Liste des instructions"></textarea>
+                    <button type="submit"> + Ajouter une recette </button>
                 </form>
             </div>
         )
