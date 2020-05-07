a3299a84e4d5a2919d0b8f9d06759ae2eb854258
benaor <benjamingirard25@gmail.com>
2020-05-07 23:20:39 +0200

    sync recette and firebase

--- src/App.js
+++ src/App.js
@@ -8,10 +8,25 @@ import './App.css'
 import Header from './components/Header'
 import Admin from './components/Admin'
 import Card from './components/Card';
+
+//firebase
+import base from './base'
+
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
     recettes: {}
+  }
+
+  componentDidMount() {
+    base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    })
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref)
   }
 
   chargerExemple = () => this.setState({ recettes })
@@ -27,7 +42,7 @@ class App extends Component {
         <div className='cards'>
           {cards}
         </div>
-      <Admin chargerExemple={this.chargerExemple} />
+        <Admin chargerExemple={this.chargerExemple} />
       </div >
     )
   }
