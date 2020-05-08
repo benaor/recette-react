a7953eb88ce16c0e73c230cee45488c9fee31b4a
benaor <benjamingirard25@gmail.com>
2020-05-08 22:08:42 +0200

    create component 'Login'

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 import AjouterRecette from "./AjouterRecette"
 import AdminForm from "./AdminForm"
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import base, { firebaseApp } from '../base'
+import Login from "./Login"
 
 class Admin extends Component {
+    state = {
+        uid: null,
+        chef: null
+    }
+
+    handleAuth = (authData) => {
+        console.log(authData)
+    }
+
+    authenticate = () => {
+        firebaseApp
+        .auth()
+        .signInWithPopup('facebookAuthProvider')
+        .then(this.handleAuth)
+    }
+
     render() {
         const { recettes, ajouterRecette, majRecette, chargerExemple, supprimerRecette } = this.props
+        
+        //si l'utilisateur n'est pas connecté
+        if(!this.state.uid){
+            return <Login authenticate={this.authenticate}/>
+        }
+
         return (
             <div className="cards">
                 <AjouterRecette ajouterRecette={ajouterRecette} />
